8043f8976c85ed82f3a4e230ae570999d99daa8f
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-09-14 11:56:34 +0700

    Create AddActivity for adding new book

--- app/src/main/java/com/example/tcd0301_book_library/MainActivity.java
+++ app/src/main/java/com/example/tcd0301_book_library/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.tcd0301_book_library;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recylerView);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
