0f4d39c43888170d0a4ef16f07cb80e6f31a790d
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-05 09:52:11 +0700

    Delete a book

--- app/src/main/java/com/example/tcd0301_book_library/UpdateActivity.java
+++ app/src/main/java/com/example/tcd0301_book_library/UpdateActivity.java
@@ -1,5 +1,6 @@
 package com.example.tcd0301_book_library;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
@@ -7,14 +8,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateActivity extends AppCompatActivity {
 
     EditText titleInput, authorInput, pagesInput;
-    Button updateButton;
+    Button updateButton, deleteButton;
 
     String id, title, author, pages;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,8 @@ public class UpdateActivity extends AppCompatActivity {
         authorInput = findViewById(R.id.author_input_update);
         pagesInput = findViewById(R.id.pages_input_update);
         updateButton = findViewById(R.id.button_update);
-
+        deleteButton = findViewById(R.id.button_delete);
+        builder =  new AlertDialog.Builder(this);
         getAndSetIntentData();
 
         updateButton.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +41,13 @@ public class UpdateActivity extends AppCompatActivity {
                 pages = pagesInput.getText().toString().trim();
 
                 db.updateBook(id, title, author, pages);
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteBook();
             }
         });
     }
@@ -62,4 +73,29 @@ public class UpdateActivity extends AppCompatActivity {
             Toast.makeText(this,"NO DATA", Toast.LENGTH_SHORT).show();
         }
     }
+
+    void deleteBook(){
+        builder.setTitle("Delete " + title + " ?");
+        builder.setMessage("Are you sure ???");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                DatabaseHelper databaseHelper = new DatabaseHelper(UpdateActivity.this);
+                databaseHelper.deleteOneRow(id);
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        builder.create().show();
+    }
+
+
 }
